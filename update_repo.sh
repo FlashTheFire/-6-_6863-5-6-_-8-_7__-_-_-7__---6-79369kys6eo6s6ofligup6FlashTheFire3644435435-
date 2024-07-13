bd68cb8a8afc03b6f2a811633828fc0c0e397cdb
FlashTheFire <harshtakur001@gmail.com>
2024-07-13 09:56:31 +0530

    Create update_repo.sh

---
+++ update_repo.sh
@@ -0,0 +1,5 @@
+echo '#!/bin/bash
+
+cd FlashTheFireAlwaysOnFireDontCopyMotherHindJaiHinfuRastraJaiShreeRamJaiShreeHanuman
+git pull origin main
+echo "Repository updated successfully!"' > FlashTheFireAlwaysOnFireDontCopyMotherHindJaiHinfuRastraJaiShreeRamJaiShreeHanuman/update_repo.sh
